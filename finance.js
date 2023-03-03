4322e3d0b400484f9ca9a6d4e8891271319b26f3
sudeepck <56756571+sudeepck@users.noreply.github.com>
2023-03-03 15:32:46 +0530

    Update finance.js
    using fetch instead of request

--- finance.js
+++ finance.js
@@ -2,30 +2,25 @@
 const express = require('express')
 const app = express()
 const {executeTradingStrategyForFinanace} = require('./executeTradingStrategyForFinanace')
-const request = require('request')
 const {brokerageCharges} = require('./brokerageFees');
 const {formatData} = require('./FormatData'); 
+
 
 app.get('/',(req,res)=>{
     res.send("Trading Platform")
 })
 
 app.get('/trading',async(req,res)=>{
+    const { default: fetch } = await import('node-fetch');
     let { companyTiker, averagePrice,threshold,brokerageName,start,end}= req.query
-    
     let {brokerageBuyFee,brokerageSellFee} = brokerageCharges(brokerageName);
-    let = url = `https://query1.finance.yahoo.com/v8/finance/chart/${companyTiker}?range=30d&interval=5m`; 
-       
-    request.get({url: url,json: true,headers: {'User-Agent': 'request'}}, (err, response, data) => {   
-        if (err) {
-            console.log('Error:', err);
-            console.log('Status:', response.statusCode);
-        } else {
-                let tradeMarketDataArray = formatData(data);
-                str = executeTradingStrategyForFinanace(tradeMarketDataArray,threshold,averagePrice,brokerageBuyFee,brokerageSellFee);
-                res.send(str);
-        }
-    })
+    let url = `https://query1.finance.yahoo.com/v8/finance/chart/${companyTiker}?range=30d&interval=5m`; 
+
+    const response = await fetch(url);
+    const data = await response.json();
+    let tradeMarketDataArray = formatData(data);
+    str = executeTradingStrategyForFinanace(tradeMarketDataArray,threshold,averagePrice,brokerageBuyFee,brokerageSellFee);
+    res.send(str);
 })
 
 app.listen(3000,()=>{
