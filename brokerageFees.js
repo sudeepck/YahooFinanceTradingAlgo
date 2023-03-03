c1571deea8ed20dcd5b2ad1e968ba9b287cc8068
sudeepck <56756571+sudeepck@users.noreply.github.com>
2023-03-03 15:06:37 +0530

    Update brokerageFees.js
    Added brokerage charges function

--- brokerageFees.js
+++ brokerageFees.js
@@ -21,6 +21,24 @@ const data = [
     }
 ]
 
-module.exports = {data}
+brokerageCharges = (brokerageName)=>{
+     let brokerageBuyFee = [{percentage:0.03},{amount :20}]
+    let brokerageSellFee = [{percentage:0.03},{amount :20}]
+
+    if(brokerageName){
+            data.forEach(function(item) {
+                if(item.name === brokerageName){
+                    brokerageBuyFee[0] = {"percentage" :item.brokerageBuyFee[0].percentage};
+                    brokerageBuyFee[1] = {"amount":item.brokerageBuyFee[1].amount};
+                    brokerageSellFee[0] = {"percentage" :item.brokerageBuyFee[0].percentage};
+                    brokerageSellFee[1] = {"amount":item.brokerageBuyFee[1].amount};
+                }
+            });
+        }
+    return {brokerageBuyFee,brokerageSellFee} 
+}
+
+  
+module.exports = {brokerageCharges}
 
 
