0eac10e59f232d12d98b80bbe9400f6164ae2aae
sudeepck <56756571+sudeepck@users.noreply.github.com>
2023-03-07 12:24:50 +0530

    Update finance.js

--- finance.js
+++ finance.js
@@ -1,7 +1,6 @@
 
 const express = require('express')
 const app = express()
-const {executeTradingStrategyForFinanace} = require('./executeTradingStrategyForFinanace')
 const {brokerageCharges} = require('./brokerageFees');
 const {formatData} = require('./FormatData'); 
 const path = require('path');
